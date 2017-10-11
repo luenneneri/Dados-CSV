b8804c5bb5e333805a8a66d78750b868e9536e98
Luenne Neri <luenne.neri@gmail.com>
2017-10-11 20:52:53 -0300

    Criação do arquivo csv

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace arquivocsv
 {
@@ -6,7 +7,33 @@ namespace arquivocsv
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nome, email;
+            int idade;
+
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine();
+            
+            Console.WriteLine("Digite o seu email: ");
+            email = Console.ReadLine();
+            
+            Console.WriteLine("Digite a sua idade: ");
+            idade = Int16.Parse(Console.ReadLine());
+
+            /*Pegar essas informações e escrever dentro de um arquivo*/
+            /*Primeiro vamos criar o arquivo com StreamWriter*/
+
+            StreamWriter arquivo = new StreamWriter ("dados.csv", true);
+
+            /*Vamos escrever um cadastro por linha com Writeline, e aplicando ; para adicionar o próximo dado em outra célula da mesma linha*/
+
+            arquivo.WriteLine(nome+";"+email+";"+idade+" anos"+";"+DateTime.Now.ToShortDateString());
+
+
+            arquivo.Close();
+
+
+
+
         }
     }
 }
